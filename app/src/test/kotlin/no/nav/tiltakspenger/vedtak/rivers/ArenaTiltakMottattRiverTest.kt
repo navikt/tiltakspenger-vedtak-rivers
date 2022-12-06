1b121039c1c6b33804596770fb7f58e23b885cf8
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-12-06 10:07:25 +0100

    Add test for a failing http-call to tiltakspenger-vedtak
    Co-authored-by: richardmartinsen <Richard.Martinsen@nav.no>
    Co-authored-by: haagenhasle <Hagen.Hasle@nav.no>
    Co-authored-by: erlendev <Erlend.Edvardsen.Vige@nav.no>

--- app/src/test/kotlin/no/nav/tiltakspenger/vedtak/rivers/ArenaTiltakMottattRiverTest.kt
+++ app/src/test/kotlin/no/nav/tiltakspenger/vedtak/rivers/ArenaTiltakMottattRiverTest.kt
@@ -2,17 +2,19 @@ package no.nav.tiltakspenger.vedtak.rivers
 
 import io.mockk.coEvery
 import io.mockk.coVerify
+import io.mockk.mockk
 import io.mockk.spyk
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import no.nav.tiltakspenger.vedtak.client.IVedtakClient
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 internal class ArenaTiltakMottattRiverTest {
 
     private val testRapid = TestRapid()
 
-    private val vedtakClient = spyk<IVedtakClient>()
+    private val vedtakClient = mockk<IVedtakClient>()
 
     init {
         ArenaTiltakMottattRiver(rapidsConnection = testRapid, vedtakClient = vedtakClient)
@@ -25,10 +27,21 @@ internal class ArenaTiltakMottattRiverTest {
 
     @Test
     fun `Når en løsning for arenaTiltak mottas, så videresender vi data til tiltakspenger-vedtak`() {
+        coEvery { vedtakClient.mottaTiltak(any(), "42") } returns Unit
         val arenaTiltakMottattHendelse =
             javaClass.getResource("/arenaTiltakMottattHendelse.json")?.readText(Charsets.UTF_8)!!
         testRapid.sendTestMessage(arenaTiltakMottattHendelse)
-        coEvery { vedtakClient.mottaTiltak(any(), any()) } returns Unit
-        coVerify { vedtakClient.mottaTiltak(any(), any()) }
+        coVerify { vedtakClient.mottaTiltak(any(), "42") }
+    }
+
+    @Test
+    fun `Når en løsning for arenaTiltak mottas, så videresender vi data til tiltakspenger-vedtak som feiler`() {
+        coEvery { vedtakClient.mottaTiltak(any(), "42") } throws RuntimeException()
+        val arenaTiltakMottattHendelse =
+            javaClass.getResource("/arenaTiltakMottattHendelse.json")?.readText(Charsets.UTF_8)!!
+        assertThrows<RuntimeException> {
+            testRapid.sendTestMessage(arenaTiltakMottattHendelse)
+        }
+        coVerify { vedtakClient.mottaTiltak(any(), "42") }
     }
 }
