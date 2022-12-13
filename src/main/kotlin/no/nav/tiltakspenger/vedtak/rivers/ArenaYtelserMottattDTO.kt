2582df6cba47c28866529c357dba0dfbb29cbffa
Richard Martinsen <github@martinsen.org>
2022-12-13 11:05:42 +0100

    ArenaYtelserMottattDTO skal ikke være nullable

--- src/main/kotlin/no/nav/tiltakspenger/vedtak/rivers/ArenaYtelserMottattDTO.kt
+++ src/main/kotlin/no/nav/tiltakspenger/vedtak/rivers/ArenaYtelserMottattDTO.kt
@@ -3,7 +3,7 @@ package no.nav.tiltakspenger.vedtak.rivers
 import java.time.LocalDateTime
 
 data class ArenaYtelserMottattDTO(
-    val ytelser: List<YtelseSakDTO>?,
+    val ytelser: List<YtelseSakDTO>,
     val ident: String,
     val journalpostId: String,
     val innhentet: LocalDateTime,
