6f28857fff420ec823d4a8d1b6577d67bd485038
karlevaldnav <karl.evald.wigestrand@nav.no>
2024-02-23 10:16:41 +0100

    B Rett opp i navngivningen til BrevDTO sine felter så de matcher det som blir sendt fra vedtak, fordi dette ble endret under review i vedtak
    Co-authored-by: benedihm <benedicte.helen.myrvoll@nav.no>

--- src/main/kotlin/no/nav/tiltakspenger/vedtak/rivers/vedtaksbrev/VedtaksBrevRiver.kt
+++ src/main/kotlin/no/nav/tiltakspenger/vedtak/rivers/vedtaksbrev/VedtaksBrevRiver.kt
@@ -51,8 +51,8 @@ internal class VedtaksBrevRiver(
 }
 
 class BrevDTO(
-    val personalia: Personalia,
-    val tiltaksinfo: Tiltaksinfo,
+    val personaliaDTO: PersonaliaDTO,
+    val tiltaksinfoDTO: TiltaksinfoDTO,
     val fraDato: String,
     val tilDato: String,
     val saksnummer: String,
@@ -62,7 +62,7 @@ class BrevDTO(
     val innsendingTidspunkt: LocalDateTime,
 )
 
-data class Personalia(
+data class PersonaliaDTO(
     val dato: String,
     val ident: String,
     val fornavn: String,
@@ -75,7 +75,7 @@ data class Personalia(
     val antallBarn: Int,
 )
 
-data class Tiltaksinfo(
+data class TiltaksinfoDTO(
     val tiltak: String,
     val tiltaksnavn: String,
     val tiltaksnummer: String,
