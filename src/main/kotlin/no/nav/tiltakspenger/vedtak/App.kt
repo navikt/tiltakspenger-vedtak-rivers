fc63e565cd25f3da715350f7d016b2a5069d96be
Hågen Hasle <haagenhasle@gmail.com>
2024-06-24 16:08:02 +0200

    Fjernet rivers for å håndtere de meldingene vi ikke lenger skal ha med som del av Innsending-flyten

--- src/main/kotlin/no/nav/tiltakspenger/vedtak/App.kt
+++ src/main/kotlin/no/nav/tiltakspenger/vedtak/App.kt
@@ -8,18 +8,14 @@ import no.nav.tiltakspenger.vedtak.client.MeldekortClient
 import no.nav.tiltakspenger.vedtak.client.VedtakClient
 import no.nav.tiltakspenger.vedtak.oauth.AzureTokenProvider
 import no.nav.tiltakspenger.vedtak.rivers.events.DayHasBegunRiver
-import no.nav.tiltakspenger.vedtak.rivers.foreldrepenger.ForeldrepengerMottattRiver
 import no.nav.tiltakspenger.vedtak.rivers.innsending.InnsendingUtdatertRiver
 import no.nav.tiltakspenger.vedtak.rivers.meldekort.MeldekortGrunnlagRiver
-import no.nav.tiltakspenger.vedtak.rivers.overgangsstønad.OvergangsstønadMottattRiver
 import no.nav.tiltakspenger.vedtak.rivers.personopplysninger.PersonopplysningerMottattRiver
 import no.nav.tiltakspenger.vedtak.rivers.skjerming.SkjermingMottattRiver
 import no.nav.tiltakspenger.vedtak.rivers.søknad.SøknadMottattRiver
 import no.nav.tiltakspenger.vedtak.rivers.tiltak.ArenaTiltakMottattRiver
 import no.nav.tiltakspenger.vedtak.rivers.tiltak.TiltakMottattRiver
-import no.nav.tiltakspenger.vedtak.rivers.uføre.UføreMottattRiver
 import no.nav.tiltakspenger.vedtak.rivers.vedtaksbrev.VedtaksBrevRiver
-import no.nav.tiltakspenger.vedtak.rivers.ytelser.ArenaYtelserMottattRiver
 
 fun main() {
     System.setProperty("logback.configurationFile", Configuration.logbackConfigurationFile())
@@ -32,9 +28,12 @@ fun main() {
         securelog.error(e) { e.message }
     }
 
-    val meldekortTokenProvider = AzureTokenProvider(config = Configuration.oauthConfig(scope = Configuration.meldekortScope()))
-    val dokumentTokenProvider = AzureTokenProvider(config = Configuration.oauthConfig(scope = Configuration.dokumentScope()))
-    val vedtakTokenProvider = AzureTokenProvider(config = Configuration.oauthConfig(scope = Configuration.vedtakScope()))
+    val meldekortTokenProvider =
+        AzureTokenProvider(config = Configuration.oauthConfig(scope = Configuration.meldekortScope()))
+    val dokumentTokenProvider =
+        AzureTokenProvider(config = Configuration.oauthConfig(scope = Configuration.dokumentScope()))
+    val vedtakTokenProvider =
+        AzureTokenProvider(config = Configuration.oauthConfig(scope = Configuration.vedtakScope()))
 
     val vedtakClient = VedtakClient(
         getToken = vedtakTokenProvider::getToken,
@@ -52,11 +51,6 @@ fun main() {
     }
     val rapidsConnection: RapidsConnection = RapidApplication.Builder(rapidConfig).build()
     rapidsConnection.apply {
-        OvergangsstønadMottattRiver(
-            rapidsConnection = this,
-            vedtakClient = vedtakClient,
-        )
-
         SøknadMottattRiver(
             rapidsConnection = this,
             vedtakClient = vedtakClient,
@@ -78,17 +72,7 @@ fun main() {
             vedtakClient = vedtakClient,
         )
 
-        ArenaYtelserMottattRiver(
-            rapidsConnection = this,
-            vedtakClient = vedtakClient,
-        )
-
         PersonopplysningerMottattRiver(
-            rapidsConnection = this,
-            vedtakClient = vedtakClient,
-        )
-
-        ForeldrepengerMottattRiver(
             rapidsConnection = this,
             vedtakClient = vedtakClient,
         )
@@ -101,11 +85,6 @@ fun main() {
         VedtaksBrevRiver(
             rapidsConnection = this,
             dokumentClient = dokumentClient,
-        )
-
-        UføreMottattRiver(
-            rapidsConnection = this,
-            vedtakClient = vedtakClient,
         )
 
         SkjermingMottattRiver(
